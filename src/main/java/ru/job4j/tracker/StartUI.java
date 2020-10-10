abd813cff8e463665de592cbb403f4fd143688eb
AnnWhite007 <anuta_myrzilka@inbox.ru>
2020-10-10 16:48:53 +0300

    6. Date. Отображение даты. [#371418]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+/**
+ * 2.6. Date. Отображение даты.
+ * Работа с датами с помощью классов и методов, которые находятся в пакете java.time,
+ * поэтому чтобы их использовать необходимо импортировать этот пакет.
+ * LocalDate, LocalTime, LocalDateTime
+ * Метод now() - показывает текущее значение
+ * DateTimeFormatter - класс который с помощью определенных в нем методов
+ * позволяет отформатировать дату в соответствии с образцом
+ * Полный перечень принятых сокращений можно найти в документации:
+ * https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
+ */
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        System.out.println(item.getCreated().format(formatter));
+    }
+}
